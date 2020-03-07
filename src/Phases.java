d4c1ae6cdfe1995f6dd03fd87a9495cff1cc219d
Miguel Gonzalez <migonzalez@unm.edu>
2020-03-07 12:34:47 -0700

    Updated version of Phase.java

--- src/Phases.java
+++ src/Phases.java
@@ -1,4 +1,6 @@
 import Primary.Lanes;
+
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import static Primary.Lanes.*;
@@ -12,13 +14,13 @@ public enum Phases {
     EW_LEFT_RED(),
     NS_GREEN(),
     EW_GREEN(),
-    ALL_RED(new LinkedList<Lanes>(N1, N2, N3, E1,E2,E3, W1,W2,W3, S1, S2, S3), null, 3);
+    ALL_RED(new LinkedList<Lanes>(Arrays.asList(N1, N2, N3, E1, E2, E3, W1, W2, W3, S1, S2, S3)), null, 3, false, false);
 
     private int phaseTime;
     LinkedList<Lanes> redLanes = new LinkedList<Lanes>();
     LinkedList<Lanes> greenLanes = new LinkedList<Lanes>();
 
-    Phases (LinkedList<Lanes> redLanes,LinkedList<Lanes> greenLanes, int phaseTime) {
+    Phases (LinkedList<Lanes> redLanes,LinkedList<Lanes> greenLanes, int phaseTime, boolean nsPedestrians, boolean ewPedestrians) {
         this.phaseTime = phaseTime;
         this.redLanes = redLanes;
         this.greenLanes = greenLanes;
