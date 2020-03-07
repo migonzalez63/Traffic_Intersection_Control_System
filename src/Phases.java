543b219cd7a0edc9a91047e7b96f269c5b381020
danielmiller4185 <dmiller23@unm.edu>
2020-03-07 12:55:17 -0700

    changes to Phases.java

--- src/Phases.java
+++ src/Phases.java
@@ -10,9 +10,20 @@ public enum Phases {
     NS_LEFT_YELLOW(),
     NS_LEFT_RED(),
     // only E1 and W1 green
-    EW_LEFT_GREEN(new LinkedList<Lanes>(Arrays.asList(N1, N2, N3, E2, E3, W2, W3, S1, S2,S3)),new LinkedList<Lanes>(Arrays.asList(E1, W1)),3),
-    EW_LEFT_YELLOW(),
-    EW_LEFT_RED(),
+    EW_LEFT_GREEN(
+        new LinkedList<Lanes>(Arrays.asList(N1, N2, N3, E2, E3, W2, W3, S1, S2,S3)),
+        new LinkedList<Lanes>(Arrays.asList(E1, W1)), 
+        null,
+        3,
+        false,
+        false),
+    EW_LEFT_YELLOW(
+        new LinkedList<Lanes>(Arrays.asList(N1, N2, N3, E2, E3, W2, W3, S1, S2, S3)), 
+        null,
+        new LinkedList<Lanes>(Arrays.asList(E1, W1)),
+        3,
+        false,
+        false),
     NS_GREEN(),
     EW_GREEN(),
     ALL_RED(new LinkedList<Lanes>(Arrays.asList(N1, N2, N3, E1, E2, E3, W1, W2, W3, S1, S2, S3)), null, 3, false, false);
